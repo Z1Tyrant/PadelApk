9e903ce66d98cf3118a0641c12d0edc0112f6a32
Z1Tyrant <brya.contreras@duocuc.cl>
2023-12-12 04:55:09 -0300

    update

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -23,25 +23,49 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  async login(){
-    const loggedIn = await this.authservice.login(this.email, this.password);
-    if(loggedIn){
-      this.nacCtrl.navigateRoot('home')
+  async login() {
+    const isEmailValid = this.validateEmail(this.email);
+    const isPasswordValid = this.validatePassword(this.password);
 
-    }else{
-      this.presentAlert('Credenciales incorrectas', 'Por favor, verifica tu correo y contraseña.');
-
+    if (isEmailValid && isPasswordValid) {
+      const loggedIn = await this.authservice.login(this.email, this.password);
+      if (loggedIn) {
+        this.nacCtrl.navigateRoot('home');
+      } else {
+        this.presentAlert('Credenciales incorrectas', 'Por favor, verifica tu correo y contraseña.');
+      }
+    } else {
+      this.presentAlert('Formulario no válido', 'Por favor, completa correctamente todos los campos.');
     }
   }
 
-  async register(){
-    const registered = await this.authservice.register(this.email, this.password);
-    if(registered){
-      this.presentAlert('Usuario registrado', 'Ya puede iniciar sesion con su usuario.');
+  async register() {
+    const isEmailValid = this.validateEmail(this.email);
+    const isPasswordValid = this.validatePassword(this.password);
 
-    } else{
-      console.log('error al registrar');
+    if (isEmailValid && isPasswordValid) {
+      const registered = await this.authservice.register(this.email, this.password);
+      if (registered) {
+        this.presentAlert('Usuario registrado', 'Ya puede iniciar sesion con su usuario');
+      } else {
+        console.log('Error al registrar');
+      }
+    } else {
+      this.presentAlert('Formulario no válido', 'Por favor, completa correctamente todos los campos.');
     }
+  }
+
+  validateEmail(email: string): boolean {
+    // Puedes implementar tu propia lógica de validación de correo electrónico aquí
+    // Por ejemplo, puedes utilizar expresiones regulares o una lógica más específica según tus requisitos.
+    // En este ejemplo, simplemente verifica si el correo contiene un '@'.
+    return email.includes('@');
+  }
+
+  validatePassword(password: string): boolean {
+    // Validación de contraseña: al menos una mayúscula, dos números y longitud mínima de 8 caracteres
+    const passwordRegex = /^(?=.*[A-Z])(?=(?:.*\d){2}).{8,}$/;
+    return passwordRegex.test(password);
   }
 
   async presentAlert(header: string, message: string) {
@@ -53,5 +77,4 @@ export class LoginPage implements OnInit {
 
     await alert.present();
   }
-
 }
